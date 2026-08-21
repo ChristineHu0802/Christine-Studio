8bfabdad3e954c8edf1c836fdec861c11fd47b64
orz80i1345 <bin97624@gmail.com>
2026-08-22 06:50:28 +0800

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
+process.env.__VITE_ADDITIONAL_SERVER_ALLOWED_HOSTS = '.e2b.app,.pages.dev,christinehu.co,.christinehu.co';
+
 export default {
   server: {
-    allowedHosts: ['.e2b.app']
+    allowedHosts: ['.e2b.app', '.pages.dev', 'christinehu.co', '.christinehu.co']
   }
 };
